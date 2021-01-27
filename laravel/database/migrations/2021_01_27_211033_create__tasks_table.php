198c60edd7a836385be63f309ff476e89f8687be
sourceCodeISA <isabel.gogov1@libero.it>
2021-01-27 22:18:09 +0100

    tabella tasks

--- laravel/database/migrations/2021_01_27_211033_create__tasks_table.php
+++ laravel/database/migrations/2021_01_27_211033_create__tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeesTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-        Schema::create('_employees', function (Blueprint $table) {
-            $table -> id();
+        Schema::create('_tasks', function (Blueprint $table) {
+            $table->id();
 
             $table -> string('name', 100);
-            $table -> string('lastname', 100);
-            $table -> timestamp('date_of_birth');
-            $table -> integer('RAL');
+            $table -> string('description', 160);
+            $table -> time('hours');
 
-            $table -> timestamps();
+            $table->timestamps();
         });
     }
 
@@ -32,6 +31,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_employees');
+        Schema::dropIfExists('_tasks');
     }
 }
